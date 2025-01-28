d9017ee4c6dc052ab11a77ea7d263d1f54633407
simonleewaters <79975158+simonleewaters@users.noreply.github.com>
2025-01-28 10:39:02 +0000

    Update OpenSearchClientFactory.cs

--- FinLegal.Search.Shared/OpenSearch/OpenSearchClientFactory.cs
+++ FinLegal.Search.Shared/OpenSearch/OpenSearchClientFactory.cs
@@ -12,6 +12,7 @@ public class OpenSearchClientFactory(IConfiguration configuration) : IOpenSearch
         var uri = configuration.GetValue<string>("OPENSEARCH_URI") ?? "";
         var pool = new SingleNodeConnectionPool(new Uri(uri));
         var connectionSettings = new ConnectionSettings(pool, sourceSerializer: JsonNetSerializer.Default);
+        
         return new OpenSearchClient(connectionSettings);
     }
 }

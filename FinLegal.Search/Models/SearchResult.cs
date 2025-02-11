48800dba98ae8e3b8bf14f6b28d4186c2b8895b0
simonleewaters <79975158+simonleewaters@users.noreply.github.com>
2025-02-11 11:01:11 +0000

    Update SearchResult.cs

--- FinLegal.Search/Models/SearchResult.cs
+++ FinLegal.Search/Models/SearchResult.cs
@@ -5,6 +5,7 @@ public class SearchResult<T> where T : class
     public long Count { get; set; }
     public required List<T> Results { get; set; }
     public int Page { get; set; }
+    
 
     public static SearchResult<T> EmptyResult => new() { Count = 0, Results = [], Page = 1 };
 }
